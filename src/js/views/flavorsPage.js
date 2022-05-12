2a5bc1cdd95452a04e16170e34026ac114002dd4
jeffreyally <jeffreyally@gmail.com>
2022-05-12 12:35:55 +0000

    update

--- src/js/views/flavorsPage.js
+++ src/js/views/flavorsPage.js
@@ -10,19 +10,19 @@ export const FlavorsPage = () => {
             fontSize:'3rem'}}><i>OUR FLAVORS</i></p>
             <br></br>
             
-            <span style={{fontSize:'2.25rem',fontFamily:'Roboto sans-serif'}}><i>Classic Vanilla Sugar Cookie </i> </span>
+            <span style={{fontSize:'2.25rem', fontFamily: ' Bebas Neue, cursive'}}>Classic Vanilla Sugar Cookie  </span>
             <p style={{fontFamily:'Roboto sans-serif',fontSize:'1.2rem'}}>A familiar time honored sweet vanilla flavor, sugary and soft with each bite.</p>
 
-            <span style={{fontSize:'2.25rem',fontFamily:'Roboto sans-serif'}}><i>Strawberry Cheesecake Sugar Cookie </i></span>
+            <span style={{fontSize:'2.25rem',fontFamily: ' Bebas Neue, cursive'}}>Strawberry Cheesecake Sugar Cookie </span>
             <p style={{fontFamily:'Roboto sans-serif',fontSize:'1.2rem'}}>A sweet take on a strawberry cheesecake, taste hints of cream cheese and graham cracker with lots of strawberry flavor and chunks  of freeze dried strawberries.</p>
             <br></br>
-            <span style={{fontSize:'2.25rem',fontFamily:'Roboto sans-serif'}}><i>Chocolate Sugar Cookies </i></span>
+            <span style={{fontSize:'2.25rem',fontFamily: ' Bebas Neue, cursive'}}>Chocolate Sugar Cookies </span>
             <p style={{fontFamily:'Roboto sans-serif',fontSize:'1.2rem'}}>A rich and sweet chocolate taste in a soft sugary cookie, will kind of remind you of a brownie.</p>
             <br></br>
-            <span style={{fontSize:'2.25rem',fontFamily:'Roboto sans-serif'}}><i>Cookies and Cream Sugar Cookies </i></span>
+            <span style={{fontSize:'2.25rem',fontFamily: ' Bebas Neue, cursive'}}>Cookies and Cream Sugar Cookies </span>
             <p style={{fontFamily:'Roboto sans-serif',fontSize:'1.2rem'}}>A soft and creamy sugar cookie with chunks of Oreo cookies making for one flavorful bite after another.</p>
             <br></br>
-            <span style={{fontSize:'2.25rem',fontFamily:'Roboto sans-serif'}}><i>Lemon Sugar Cookies </i></span>
+            <span style={{fontSize:'2.25rem',fontFamily: ' Bebas Neue, cursive'}}>Lemon Sugar Cookies </span>
             <p style={{fontFamily:'Roboto sans-serif',fontSize:'1.2rem'}}>A citrusy and sweet cookie making every bite feel so refreshing and light in your mouth.</p>
 
 
